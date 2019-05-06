84a31c0bdf5c615ace775e5ce4e966395678155f
Jonathan Cai <caimjonathan@gmail.com>
2019-05-05 21:42:06 -0700

    progress on dropzone uploading files

---
+++ public/js/main.js
@@ -0,0 +1,89 @@
+// console.log("we out here!")
+
+// const selectedFile = document.getElementById('input').files[0];
+// console.log(selectedFile)
+
+// function handleFiles(files) {
+//     console.log("handling files")
+//     console.log(files)
+// }
+
+// // Dropzone object
+var dropzone = new Dropzone('#demo-upload', {
+    // previewTemplate: document.querySelector('#preview-template').innerHTML,
+    parallelUploads: 2,
+    thumbnailHeight: 120,
+    thumbnailWidth: 120,
+    maxFilesize: 3,
+    filesizeBase: 1000,
+    thumbnail: function(file, dataUrl) {
+      if (file.previewElement) {
+        file.previewElement.classList.remove("dz-file-preview");
+        var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+        for (var i = 0; i < images.length; i++) {
+          var thumbnailElement = images[i];
+          thumbnailElement.alt = file.name;
+          thumbnailElement.src = dataUrl;
+        }
+        setTimeout(function() { file.previewElement.classList.add("dz-image-preview"); }, 1);
+      }
+    }
+});
+  
+// Now fake the file upload, since GitHub does not handle file uploads
+// and returns a 404
+
+var minSteps = 6,
+    maxSteps = 60,
+    timeBetweenSteps = 100,
+    bytesPerStep = 100000;
+
+dropzone.uploadFiles = function(files) {
+    var self = this;
+    // console.log(files[0].getAsDataURL())
+    var reader  = new FileReader()
+    reader.readAsDataURL(files[0])
+    // console.log(files[0].result)
+    reader.onload = function () {
+        console.log(reader.result);
+        var str = reader.result
+        // var src = str.slice(str.lastIndexOf('/') + str.length)
+        // console.log(src)
+        $(".img").attr("src", str)
+        $(".res-para").text("this is the result of the ocr")
+
+      };
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    // var lmao = reader.readAsDataURL(files[0])
+    // console.log(lmao)
+
+    for (var i = 0; i < files.length; i++) {
+
+        var file = files[i];
+        totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+
+        for (var step = 0; step < totalSteps; step++) {
+        var duration = timeBetweenSteps * (step + 1);
+        setTimeout(function(file, totalSteps, step) {
+            return function() {
+            file.upload = {
+                progress: 100 * (step + 1) / totalSteps,
+                total: file.size,
+                bytesSent: (step + 1) * file.size / totalSteps
+            };
+
+            self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+            if (file.upload.progress == 100) {
+                file.status = Dropzone.SUCCESS;
+                self.emit("success", file, 'success', null);
+                self.emit("complete", file);
+                self.processQueue();
+                //document.getElementsByClassName("dz-success-mark").style.opacity = "1";
+            }
+            };
+        }(file, totalSteps, step), duration);
+        }
+    }
+}
